9067be182dc304141ffbdef6afedccc7ddc2b8dd
ChemzRegaton <112966149+Jedlouies@users.noreply.github.com>
2026-08-25 15:43:51 +0800

    update

--- barons-web/bootstrap/app.php
+++ barons-web/bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'supabase.session' => \App\Http\Middleware\CheckSupabaseSession::class,
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Illuminate\Session\TokenMismatchException $e, $request) {
             Auth::logout();
